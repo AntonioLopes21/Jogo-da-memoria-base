5fbaa4fedef4175dd393945278926910b4df7b9b
Antonio.lopes <antonyejoao@gmail.com>
2024-11-25 04:43:11 -0300

    criando eventos de salvar e redirecionar

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const input = document.querySelector(".login_input")
+const button = document.querySelector(".login_button")
+const form = document.querySelector(".login-form")
+
+function validarInput({ target }) {
+    if(target.value.length > 2 ) {
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', "")
+    }
+}
+
+function validarForm(evento) {
+    evento.preventDefault()
+    
+    //vai salvar chave valor
+    localStorage.setItem('player', input.value)
+    window.location = 'pages/game.html'
+    
+    
+    
+}
+
+input.addEventListener('input', validarInput)
+form.addEventListener('submit', validarForm)
